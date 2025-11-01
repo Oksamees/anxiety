2736eea4c40febe34360a812e791249de8605dc1
Oksamees <93395289+Oksamees@users.noreply.github.com>
2025-11-01 16:34:50 +0200

    Update Options.js

--- scripts/game/Options.js
+++ scripts/game/Options.js
@@ -26,7 +26,7 @@ Loader.addSounds([
 			HOW_MANY_PROMPTS = 1;
 		}
 		Game.CLICK_TO_ADVANCE = !Game.CLICK_TO_ADVANCE;
-		text_automatic_toggle.innerHTML = Game.CLICK_TO_ADVANCE ? "on click" : "automatically";
+		text_automatic_toggle.innerHTML = Game.CLICK_TO_ADVANCE ? "klikkimisel" : "automaatselt";
 
 		// Sound
 		sfx( Game.CLICK_TO_ADVANCE ? "ui_button2" : "ui_button1");
